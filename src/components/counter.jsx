dc9caad476de6c0ea8319123f59243a3e048d0a2
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-04-24 01:05:15 +0530

    Modify class to className

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,8 +9,8 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span class={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button class="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key="tag">{tag}</li>
