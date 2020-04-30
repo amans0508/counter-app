97362a67be9a52e3cfb67f72df641233c3c9bc7e
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-04-30 08:14:23 +0530

    Obj Destructuring #2

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,13 +3,13 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
-    const { onReset, onDelete, onIncrement } = this.props;
+    const { onReset, onDelete, onIncrement, counter } = this.props;
     return (
       <div>
         <button className="btn-primary btn-small m-2" onClick={onReset}>
           Reset
         </button>
-        {this.props.counter.map((counter) => (
+        {counter.map((counter) => (
           <Counter
             key={counter.id}
             onDelete={onDelete}
